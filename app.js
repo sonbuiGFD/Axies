6243bfd17002c5734a59f9103ec856fff01f9d8e
son bui <sonbui@linecorp.com>
2023-04-10 18:01:49 +0700

    update ranking and activeiy

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-// var sassMiddleware = require('node-sass-middleware');
 var favicon = require('serve-favicon');
 var expressLayouts = require('express-ejs-layouts');
 
@@ -16,19 +13,9 @@ app.use(expressLayouts);
 app.set('layout', './layouts/theme');
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// app.use(
-//   sassMiddleware({
-//     src: path.join(__dirname, 'public'),
-//     dest: path.join(__dirname, 'public'),
-//     indentedSyntax: false,
-//     sourceMap: true,
-//   }),
-// );
 
 app.use(express.static(path.join(__dirname, 'public')));
 
